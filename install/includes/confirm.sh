4511ed684f70d352982f3fd1652997a5975b8e24
Igor Fortes Oliveira Lobo <igormenato@hotmail.com>
2025-05-04 16:10:06 -0300

    fix references

--- install/includes/confirm.sh
+++ install/includes/confirm.sh
@@ -5,7 +5,7 @@ else
     figlet "Installation"
 fi
 echo -e "${NONE}"
-echo "This script will install the KyDE Hyprland Starter configuration."
+echo "This script will install the KyDE Dotfiles."
 echo "NVIDIA GPUs aren't officially supported by Hyprland. But you can give it a try..."
 echo
 if gum confirm "DO YOU WANT TO START NOW?" ;then
--- install/includes/header.sh
+++ install/includes/header.sh
@@ -5,14 +5,14 @@ NONE='\033[0m'
 
 echo -e "${GREEN}"
 cat <<"EOF"
- _   _                  _                 _ 
+ _   _                  _                 _
 | | | |_   _ _ __  _ __| | __ _ _ __   __| |
 | |_| | | | | '_ \| '__| |/ _` | '_ \ / _` |
 |  _  | |_| | |_) | |  | | (_| | | | | (_| |
 |_| |_|\__, | .__/|_|  |_|\__,_|_| |_|\__,_|
-       |___/|_|                             
+       |___/|_|
 
-KyDE HYPRLAND STARTER PACKAGE
+KyDE DOTFILES PACKAGE
 EOF
 echo "Version: $version"
 echo -e "${NONE}"
